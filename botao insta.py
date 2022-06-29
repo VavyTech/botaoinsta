cea55cf953e1321c26473bc3cc379fbd472ed177
VANESSA  ALMEIDA <valmeida21@hotmail.com>
2022-06-28 21:54:17 -0300

    Add files via upload
    Logo do Instagram

---
+++ botao insta.py
@@ -0,0 +1,66 @@
+
+
+
+from webbrowser import BackgroundBrowser
+from pip import main
+
+
+def test_body_decode():
+    """
+    >>> body_decode('dGVzdA==')
+    'test'
+    """
+    <html>
+    <body>
+    <h1>Hello World</h1>
+    </body>
+    </html>
+/* main box of insta */
+.insta  {
+    position: relative;
+    width: 150px;
+    height: 150px
+    Backgroun: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #d5949 45%, #d6249f 60%,#285aeb 90%);
+    border-radius: 35px;
+    display: grid;
+    place-items: center;
+}
+
+/* innerbox in insta box */
+.innerbox {
+    width: 120px;
+    height:120px;
+    border: 10px solid #fff;
+    border-radius:32px;
+    display: grid;
+    place-items: center;
+}
+/* center circle of insta */
+.innerbox::before {
+    content: ";
+    width: 45px;
+    heigth:45px;
+    border:10px solid #fff;
+    border-radius:50%;
+    backgroud: transparent;
+    position: absolute;
+    }
+    /* top right circle of insta */
+    .innerbox::after {
+        cintent: ";
+        width: 10px;
+        border:2px solid #fff;
+        border-radius: 50%;
+        backgroud: #fff;
+        position: absolute;
+        top: 8px;
+        right: 10px;
+        }
+        "
+}
+}
+
+}
+
+
+
